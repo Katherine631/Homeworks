b9d6723d4873f709c7465112f54a44ab3c152d44
Katherine631 <Katy_631@mail.ru>
2020-11-22 23:07:51 +0300

    end work

---
+++ C#OOP/C_OOP/C_OOP/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_OOP
+{
+    abstract class Book
+    {
+        public string name;
+        public string author;
+        public int year;
+
+
+        public Book(string name, string author, int year)
+        {
+            this.name = name;
+            this.author = author;
+            this.year = year;
+        }
+
+        abstract public void Open(); //открыть книгу
+        public virtual void contents() // открыть оглавление
+        {
+            Console.WriteLine("Оглавление открыто");
+        }
+
+    }
+}
