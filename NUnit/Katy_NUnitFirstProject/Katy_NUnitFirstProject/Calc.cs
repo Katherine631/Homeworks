03335bb12a4e27c970829a6b3d9120098ce38fc7
Katherine631 <Katy_631@mail.ru>
2020-12-06 19:12:55 +0300

    end work

---
+++ NUnit/Katy_NUnitFirstProject/Katy_NUnitFirstProject/Calc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katy_NUnitFirstProject
+{
+    static public class Calc
+    {
+        static public int Plus(int x, int y)
+        {
+            return x + y;
+        }
+
+        static public int Minus(int x, int y)
+        {
+            return x - y;
+        }
+        static public int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+        static public int Division(int x, int y)
+        {
+            if (y == 0)
+                return 0;
+            return x / y;
+        }
+    }
+}
