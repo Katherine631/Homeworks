86602e208b2ad7597a6335cf5dd49ac21719a861
Katherine631 <Katy_631@mail.ru>
2020-12-19 20:03:25 +0300

    end of work

---
+++ Frameworks/NUnitTestProject1/PO/ExistProductPage.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTestProject1.PO
+{
+    class ExistProductPage : OpenedProductPage
+    {
+        public ExistProductPage(IWebDriver driver) : base(driver)
+        {
+            this.driver = driver;
+        }
+        public string ProductNameValue()
+        {
+            return FldProductName.GetAttribute("value");
+        }
+        public string CategoryIdValue()
+        {
+            return FldCategoryId.GetAttribute("value");
+        }
+        public string SupplierIdValue()
+        {
+            return FldSupplierId.GetAttribute("value");
+        }
+        public string UnitPriceValue()
+        {
+            return FldUnitPrice.GetAttribute("value");
+        }
+        public string QuantityPerUnitValue()
+        {
+            return FldQuantityPerUnit.GetAttribute("value");
+        }
+        public string UnitsInStockValue()
+        {
+            return FldUnitsInStock.GetAttribute("value");
+        }
+        public string UnitsOnOrderValue()
+        {
+            return FldUnitsOnOrder.GetAttribute("value");
+        }
+        public string ReorderLevelValue()
+        {
+            return FldReorderLevel.GetAttribute("value");
+        }
+        public string DiscontinuedValue()
+        {
+            return FldDiscontinued.GetAttribute("checked");
+        }
+        public void ReturnToProductsPage()
+        {
+            driver.FindElement(By.LinkText("All Products")).Click();
+        }
+        
+
+    }
+}
