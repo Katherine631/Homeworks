e03a98dab16c898357f66fbb964dcdf34fe52527
Katherine631 <Katy_631@mail.ru>
2020-12-22 22:10:59 +0300

    end of work

---
+++ SpeckFlow/NUnitTestProject1/features/SpecFlowFeature1.feature
@@ -0,0 +1,14 @@
+﻿Feature: FishTest
+	As a user 
+	I want to login on URL http://localhost:5000/
+	So I can create a product and make sure it exists
+
+
+Scenario: Create product
+	Given I open "http://localhost:5000/" url 
+	When I login with "user", "user" fields
+	And I click All Products link
+	And I click Create new button
+	And I fill next fields "Fish", "Seafood", "Tokyo Traders", "7", "1", "10", "3", "1"
+	And I click Send button
+	Then The product "Fish" should be created
---
+++ SpeckFlow/NUnitTestProject1/step_definitions/FishSteps.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using NUnitTestProject1.business_objects;
+using NUnitTestProject1.PO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace NUnitTestProject1.step_definitions
+{
+    [Binding]
+
+    public class FishSteps
+    {
+        private IWebDriver driver;
+        
+
+        [Given(@"I open ""(.+)"" url")]
+        public void GivenIOpenLoginPage(string url)
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+        }
+
+        [When(@"I login with ""(.+)"", ""(.+)"" fields")]
+        public void WhenILogin(string name, string password)
+        {
+            new LoginPage(driver).TypeUserInLoginPage(name, password);
+            new LoginPage(driver).Login();
+        }
+
+        [When(@"I click All Products link")]
+        public void WhenIClickAllProductslink()
+        {
+            new HomePage(driver).ProductsPage();
+        }
+
+        [When(@"I click Create new button")]
+        public void WhenIClickCreateNewButton()
+        {
+            new ProductsPage(driver).OpenNewProduct();
+        }
+
+        [When(@"I fill next fields ""(.+)"", ""(.+)"", ""(.+)"", ""(.+)"", ""(.+)"", ""(.+)"", ""(.+)"", ""(.+)""")]
+        public void WhenIFillNextFields(string ProductName, string CathegoryId, string SupplierId, string UnitPrice, string QuantityPerUnit, string UnitsInStock, string UnitsInOrder, string ReorderLevel)
+        {
+            Product product = new Product(ProductName, CathegoryId, SupplierId, UnitPrice, QuantityPerUnit, UnitsInStock, UnitsInOrder, ReorderLevel);
+            new NewProductPage(driver).CreateNewProduct(product);
+        }
+
+        [When(@"I click Send button")]
+        public void WhenIClickCreateSendButton()
+        {
+            new NewProductPage(driver).ClickSendButton();
+        }
+
+        [Then(@"The product ""(.+)"" should be created")]
+        public void ThenTheProductShouldBeCreated(string name)
+        {
+            ProductsPage productsPage = new ProductsPage(driver);
+            Assert.IsTrue(productsPage.FindName(name));
+        } 
+        [AfterScenario]
+        public void CloseDriver()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
